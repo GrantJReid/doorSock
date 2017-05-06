04570e857f71185d7843592e41a7c88c1c882e67
GrantJReid <gjreid1s@semo.edu>
2017-05-06 12:14:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+i = 1;
+function changePic()
+{
+  if(document.getElementById("imageChange").src=="http://imgur.com/yiHc543.gif")
+    document.getElementById("imageChange").src=""; 
+  else
+    document.getElementById("imageChange").src="http://imgur.com/yiHc543.gif";
+}
