6f93412a543e6f8f7855e9d2475f07a10d8b6472
GrantJReid <gjreid1s@semo.edu>
2017-05-06 12:38:26 -0500

    Removing useless code + trying to fix open.jpg

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-i = 1;
 function changePic()
 {
   if(document.getElementById("imageChange").src=="http://imgur.com/yiHc543.gif")
-    document.getElementById("imageChange").src="open.jpg"; 
+    document.getElementById("imageChange").src="/open.jpg"; 
   else
     document.getElementById("imageChange").src="http://imgur.com/yiHc543.gif";
 }
