cdb8ac49308abdaaa2987ee625ff513b846e44ea
GrantJReid <gjreid1s@semo.edu>
2017-05-06 12:40:46 -0500

    Giving up and user imgur
    Standard coding practices be damned, this joke is worth it

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function changePic()
 {
   if(document.getElementById("imageChange").src=="http://imgur.com/yiHc543.gif")
-    document.getElementById("imageChange").src="/open.jpg"; 
+    document.getElementById("imageChange").src="http://i.imgur.com/iY7P8Y8.jpg"; 
   else
     document.getElementById("imageChange").src="http://imgur.com/yiHc543.gif";
 }
