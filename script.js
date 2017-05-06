d0828d632a1ae3cd617ede877184fd7182bd504d
GrantJReid <gjreid1s@semo.edu>
2017-05-06 12:32:22 -0500

    Adding open.jpg to the site

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ i = 1;
 function changePic()
 {
   if(document.getElementById("imageChange").src=="http://imgur.com/yiHc543.gif")
-    document.getElementById("imageChange").src=""; 
+    document.getElementById("imageChange").src="open.jpg"; 
   else
     document.getElementById("imageChange").src="http://imgur.com/yiHc543.gif";
 }
